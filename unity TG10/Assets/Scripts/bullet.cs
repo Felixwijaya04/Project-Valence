f92b9beee05b6eb7e0ee1c6ed4518edd8d05c103
ORIAMM <riovalgm.13@gmail.com>
2024-01-10 15:31:18 +0700

    Muzzle Flash

--- unity TG10/Assets/Scripts/bullet.cs
+++ unity TG10/Assets/Scripts/bullet.cs
@@ -8,10 +8,13 @@ public class bullet : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;
+    public Sprite FlashSprite;
+    public int FramesToFlash = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartCoroutine(DoFlash());
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody2D>();
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
@@ -34,6 +37,20 @@ public class bullet : MonoBehaviour
     {
         Destroy(gameObject);
         /*Debug.Log("PILIPPPPPPPPPPPPPPPPPPP");*/
+    }
+
+    IEnumerator DoFlash()
+    {
+        var renderer = GetComponent<SpriteRenderer>();
+        var originalSprite = renderer.sprite;
+        renderer.sprite = FlashSprite;
+
+        var framesFlashed = 0;
+        while (framesFlashed < FramesToFlash)
+        {
+            framesFlashed++;
+            yield return null;
+        }
     }
 
 }
