dd0582d8087c68c3aee03f788445bda7b70c0926
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-02-23 16:54:33 +0700

    update vending ui

---
+++ unity TG10/Assets/Scripts/VendingManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VendingManager : MonoBehaviour
+{
+    public GameObject VendingUI;
+    private bool _isplayer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VendingUI.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && _isplayer == true)
+        {
+            VendingUI.gameObject.SetActive(true);
+        }
+        if(_isplayer == false)
+        {
+            VendingUI.gameObject.SetActive(false) ;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _isplayer = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            _isplayer = false;
+        }
+    }
+}
