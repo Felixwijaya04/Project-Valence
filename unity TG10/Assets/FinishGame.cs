ff21c2de377c248b61dad24b26fe79fd989a9993
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-03-24 11:26:45 +0700

    z pos bug fixes

--- unity TG10/Assets/FinishGame.cs
+++ unity TG10/Assets/FinishGame.cs
@@ -8,6 +8,7 @@ public class FinishGame : MonoBehaviour
     public GameObject uigameObject;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Time.timeScale = 0f;
         uigameObject.SetActive(true);
     }
 }
